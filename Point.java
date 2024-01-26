82f38b16679a1a31766486c39c13c794333f0df1
Ella <146346991+meelochk@users.noreply.github.com>
2024-01-26 20:12:17 +0600

    Update Point.java

--- Point.java
+++ Point.java
@@ -1,35 +1,38 @@
-package com.fxdj;
+package com.example;
 
 import java.lang.Math;
-public class Point {
-    private double x;
-    private double y;
 
-    public Point(double x, double y) {
-        this.x = x;
-        this.y = y;
+public class MyPoint {
+    private double xCoordinate;
+    private double yCoordinate;
+
+    public MyPoint(double x, double y) {
+        this.xCoordinate = x;
+        this.yCoordinate = y;
     }
 
-    public double getX() {
-        return x;
-    }
-    public double getY() {
-        return y;
+    public double getXCord() {
+        return xCoordinate;
     }
 
-    public double distanceTo(Point destination){
-        double x1 = this.x;
-        double y1 = this.y;
-        //used Point destination to get the values of second coordinate
-        double x2 = destination.getX();
-        double y2 = destination.getY();
+    public double getYCord() {
+        return yCoordinate;
+    }
 
-        double distance = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    public double calculateDistanceTo(MyPoint destination) {
+        double x1 = this.xCoordinate;
+        double y1 = this.yCoordinate;
+
+        // Use destination to get the values of the second coordinate
+        double x2 = destination.getXCord();
+        double y2 = destination.getYCord();
+
+        double distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
         return distance;
     }
 
     @Override
     public String toString() {
-        return "(" + x + ", " + y + ")";
+        return "(" + xCoordinate + ", " + yCoordinate + ")";
     }
 }
